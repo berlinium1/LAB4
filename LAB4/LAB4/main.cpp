e450045a2fe46ca2ee64b8e951bb1ec75081cd2d
Yaroslav Berlinsky <58460556+berlinium1@users.noreply.github.com>
2020-11-10 23:38:41 +0200

    Create main.cpp

---
+++ LAB4/LAB4/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+
+using namespace std;
+int main(){
+    float x;
+    int n;
+    cout<<"Введіть значення аргумента X: "; cin>>x;
+    cout<<"\nВведіть ціле значення N, до якого плануєте розраховувати суму: "; cin>>n;
+    float Sum=0;
+    float factorial=1;
+    float expression;
+    for (int i=0; i<=n; i++){
+        for (int j=1; j<=2*i; j++){
+            factorial=factorial*j;
+        }
+        expression=pow(-1,i)*(pow(x,2*i)/factorial);
+        factorial=1;
+        Sum=Sum+expression;
+        printf("\nПри N = %-d значення виразу дорівнює:\n%-10.18f\n", i, expression);
+    }
+    printf("\nСума дорівнює %10.18f\n", Sum);
+}
