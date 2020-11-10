7d044265fb0b8211b5db8f42ae473973404bc027
Yaroslav Berlinsky <58460556+berlinium1@users.noreply.github.com>
2020-11-11 00:59:39 +0300

    Update LAB4.py

--- LAB4.py
+++ LAB4.py
@@ -1,6 +1,7 @@
 x = int(input("Введіть значення аргумента X "))
 n = int(input("Введіть ціле значення N, до якого плануєте розраховувати суму "))
 Sum=0
+expression=1
 factorial=1
 for i in range(0, n+1):
     for j in range(1,2*i+1):
@@ -8,7 +9,7 @@ for i in range(0, n+1):
     expression=(-1)**i*(x**(2*i)/factorial)
     factorial=1
     Sum=Sum+expression
-    print("При N = %-d значення суми дорівнює: %-10.18f"%(i, expression))#для зручності перевірки даних
+    print("При N = %-d значення суми дорівнює: %10.18f"%(i, expression))#для зручності перевірки даних
 print("Сума дорівнює %10.18f"%Sum)
 
 
